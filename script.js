f80097e8a136ebf0e8187ea0441664bfa56b309d
Odetunde Mubarak <mudet04@gmail.com>
2023-07-10 03:01:03 +0100

    c

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ window.addEventListener('scroll', () => {
         nav.classList.toggle('window-scrolled', window.scrollY > 0);
     }
 });
+
+gsap.fromTo(".header_right",
+  { x: -2},
+  { x: 2,repeat: -1, yoyo: true, ease: "power1.out"}
+);
