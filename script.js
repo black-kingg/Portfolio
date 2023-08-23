4ee3bf6854377a0cbb4b50f6d29e5ae8247c08cb
Odetunde Mubarak <mudet04@gmail.com>
2023-08-23 15:06:42 +0100

    #

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-window.addEventListener('scroll', () => {
-    const nav = document.querySelector('nav');
-    if (nav) {
-        nav.classList.toggle('window-scrolled', window.scrollY > 0);
-    }
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector("nav");
+  if (nav) {
+    nav.classList.toggle("window-scrolled", window.scrollY > 0);
+  }
 });
 
-gsap.fromTo(".header_right",
-  { x: -2},
-  { x: 2,repeat: -1, yoyo: true, ease: "power1.out"}
+gsap.fromTo(
+  ".header_right",
+  { x: -2 },
+  { x: 2, repeat: -1, yoyo: true, ease: "power1.out" }
 );
+
+// portfolio
+$(".gallery ul li a").click(function () {
+  var itemID = $(this).attr("href");
+  $(".gallery ul").addClass("item_open");
+  $(itemID).addClass("item_open");
+  return false;
+});
+$(".close").click(function () {
+  $(".port, .gallery ul").removeClass("item_open");
+  return false;
+});
+
+$(".gallery ul li a").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: parseInt($("#top").offset().top),
+    },
+    400
+  );
+});
